2dbc6e3f5f58cdcf1b49027ce8a06b7617d7ab8b
Mohamed Diaa <mdiea0581@gmail.com>
2024-11-23 13:26:39 +0200

    version 1.2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,32 @@
 <x-layout>
     <x-slot:title> about </x-slot:title>
     <x-slot:Dashbord>About us </x-slot:Dashbord>
+    <div>
+        <div class="space-y-2 ">
+            <div class="flex items-center">
+                <p class="text-2xl text-gray-700 font-bold ">
+                    What is new in
+                </p>
+                <span class="ml-2 text-lg text-gray-600 font-medium">v 1.2</span>
+            </div>
+        </div>
 
-    <div class="space-y-2">
-        <div class="flex items-center">
-            <p class="text-2xl text-gray-700 font-bold">
-                What is new in
-            </p>
-            <span class="ml-2 text-lg text-gray-600 font-medium">v 1.1</span>
+        <div class="text-black font-bold overscroll-contain"> version (v1.1)</div>
+        <div class="text-gray-500">
+            <li class="font-bold"> every email has been droped from database please create new account </li>
+            <li> edite jobs to add tags and create with tags is add it
+            </li>
+            <li> when you click on tags on any job it will show you only jobs with the same tags </li>
+            <li> in show page it will show you the tags that you assigned it </li>
+        </div>
+        <div class="text-black font-bold overscroll-contain flex"> version (v1.2) <span class="text-blue-500 px-2"> new</span>
+        </div>
+        <div class="text-black font-bold">
+            <li class="font-bold"> every email has been droped from database please create new account </li>
+            <li> you have now to prespective to show jobs</li>
+            <li> tags applay in both prespectives </li>
+            <li> </li>
         </div>
     </div>
 
-
-    <li class="font-bold"> every email has been droped from database please create new account </li>
-    <li> edite jobs to add tags and create with tags is add it
-    </li>
-    <li> when you click on tags on any job it will show you only jobs with the same tags </li>
-    <li> in show page it will show you the tags that you assigned it </li>
-    </div>
 </x-layout>
